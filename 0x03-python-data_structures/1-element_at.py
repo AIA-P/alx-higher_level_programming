d8b6a5d09be374921bef1323adbae9ec566206e1
AIA-P <adeyemiibukun05@gmail.com>
2024-03-17 19:02:02 +0100

    python

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    element = my_list[idx]
-    print("element at index {} is {}".format(idx, element))
+    if idx < 0 or idx > len(my_list) - 1:
+        return 'None'
+    else:
+        return my_list[idx]
